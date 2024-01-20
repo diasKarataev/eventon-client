b699b0cdee7bbab3a42b0df1f76522e56ed2e2a2
Dias <221812@astanait.edu.kz>
2024-01-21 01:15:16 +0600

    feat: Authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC, useContext, useEffect, useState} from 'react';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {observer} from "mobx-react-lite";
+import {IUser} from "./models/iUser";
+import UserService from "./services/UserService";
+import {autorun, reaction} from "mobx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const {store} = useContext(Context);
+    const [users, setUsers] = useState<IUser[]>([]);
 
-export default App;
+    useEffect(() => {
+        console.log("useEffect is running");
+        if (localStorage.getItem('token')) {
+            store.checkAuth();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    async function getUsers() {
+        try {
+            const response = await UserService.fetchUsers();
+            setUsers(response.data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    if (store.isLoading) {
+        return <div>Загрузка...</div>
+    }
+
+    if (!store.isAuth) {
+        return (
+            <div>
+                <LoginForm/>
+                <button onClick={getUsers}>Получить пользователей</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
+            <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
+            <button onClick={() => store.logout()}>Выйти</button>
+            <div>
+                <button onClick={getUsers}>Получить пользователей</button>
+            </div>
+            {users.map(user =>
+                <div key={user.email}>{user.email}</div>
+            )}
+        </div>
+    );
+};
+
+export default observer(App);
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React, {FC, useContext, useState} from 'react';
+import {Context} from "../index";
+import {observer} from "mobx-react-lite";
+
+const LoginForm: FC = () => {
+    const [email,setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const {store} = useContext(Context);
+    return (
+        <div>
+            <input
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                type="text"
+                placeholder="email"
+            />
+            <input
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                type="text"
+                placeholder="password"
+            />
+            <button onClick={() => store.login(email, password)}>
+                Login
+            </button>
+            <button onClick={() => store.registration(email, password)}>
+                Sign up
+            </button>
+        </div>
+    );
+};
+
+export default observer(LoginForm);
